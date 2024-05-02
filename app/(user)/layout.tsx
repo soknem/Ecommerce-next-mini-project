78bbe2475d64229184a576e805f0142a97bbf2be
soknem <sokname56@gmail.com>
2024-05-02 09:34:41 +0700

    init

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-
+import "@/globals.css";
+import NavbarComponent from "@/components/navbar/NavbarComponent";
+import FooterComponent from "@/components/footer/FooterComponent";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavbarComponent/>
+        {children}</body>
+        <FooterComponent/>
     </html>
   );
 }
